b97f460070feb4f7fa19d53bd941d087dd3a0b34
Michael Edwards <EDWARDSMR@ETSU.EDU>
2020-08-29 16:47:35 -0400

    Everything for this is assuming that we are going to be using Pythia, if we need to change the server that we are working with than we just have to change the address + Oracle objects.

---
+++ SQLServer Setup/DBAccess.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using Oracle.ManagedDataAccess.Client;
+using Oracle.EntityFrameworkCore;
+
+namespace SQLServer_Setup {
+
+
+	// Calls to this object should be inside a using statement to automatically call Dispose() and close the connection
+	// Once the connection is no longer needed.
+	class DBAccess : IDisposable {
+		private OracleConnection Connection;
+		private string Server;
+		private string UID;
+		private string Password;
+
+		public DBAccess() {
+			Initialize();
+		}
+		private void Initialize() {
+			Server = @"pythia.etsu.edu:1521/csdb.etsu.edu";
+			Console.Write("Username: ");
+			UID = Console.ReadLine();
+			Console.Write("Password: ");
+			Password = NoDisplayPassword();
+			String ConnectionString = $"Data Source={Server};User Id={UID};Password={Password};";
+			Connection = new OracleConnection(ConnectionString);
+		}
+		public bool OpenConnection() {
+			try {
+				Connection.Open();
+				Console.WriteLine("Connection Successful");
+				return true;
+			} catch(OracleException ex) {
+				switch(ex.ErrorCode) {
+					case 0:
+						Console.WriteLine("Cannot connect to server.  Contact administrator");
+						break;
+
+					case -2147467259: // This error code represents an invalid username when trying to login to the database
+						Console.WriteLine("Invalid username/password, please try again");
+						break;
+				}
+				return false;
+			}
+		}
+		private bool CloseConnection() {
+			try {
+				Connection.Close();
+				Console.WriteLine("Connection closed");
+				return true;
+			} catch(OracleException ex) {
+				Console.WriteLine(ex.Message);
+				return false;
+			}
+		}
+
+
+		public List<String[]> Query(String query) {
+			List<String[]> list = new List<String[]>();
+			if(OpenConnection() == true) {
+				OracleCommand cmd = new OracleCommand(query, Connection);
+				OracleDataReader dataReader = cmd.ExecuteReader();
+				while(dataReader.Read()) {
+					String[] currObject = new String[dataReader.VisibleFieldCount];
+					for(int i = 0; i < dataReader.VisibleFieldCount; i++) {
+						currObject[i] = dataReader[i].ToString();
+					}
+					list.Add(currObject);
+				}
+				dataReader.Close();
+				cmd.Dispose();
+				return list;
+			} else {
+				return list;
+			}
+		}
+
+		private String NoDisplayPassword() {
+			String retPassword = "";
+			while(true) {
+				ConsoleKeyInfo key = Console.ReadKey(true);
+				if(key.Key == ConsoleKey.Enter) break;
+				retPassword += key.KeyChar;
+			}
+			return retPassword;
+		}
+
+		public void Dispose() {
+			CloseConnection();
+		}
+	}
+}
---
+++ SQLServer Setup/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Oracle.EntityFrameworkCore;
+using System.Linq;
+
+namespace SQLServer_Setup {
+	class Program {
+		static void Main(string[] args) {
+			//DBAccess database = new DBAccess();
+			using(DBAccess database = new DBAccess()) {
+				List<String[]> data = database.Query("SELECT * FROM BIKE_SHOP.BICYCLE");
+				Console.WriteLine(data[0]);
+			}
+			
+			
+			
+		}
+
+		static void PrintArray(String[] arr) {
+			foreach(String i in arr) {
+				Console.Write($"{i} ");
+			}
+		}
+	}
+}
