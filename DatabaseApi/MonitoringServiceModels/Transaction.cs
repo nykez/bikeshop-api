d86c56b9fdb558ad64b7dd81b812d0d74b80bc39
Max Phillips <phillipsmk2@gmail.com>
2020-10-17 13:01:32 -0400

    Added Folder for MonitoringService Models and Transaction Model.

---
+++ DatabaseApi/MonitoringServiceModels/Transaction.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatabaseApi.MonitoringServiceModels
+{
+    public class Transaction
+    {
+        public DateTime time_Stamp { get; set; }
+    }
+}
