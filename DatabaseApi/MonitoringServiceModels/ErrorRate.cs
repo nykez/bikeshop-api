86de59242f91338f918cb3df44e233ca09fec453
Max Phillips <phillipsmk2@gmail.com>
2020-10-17 13:03:06 -0400

    Created Login and ErrorRate MSM

---
+++ DatabaseApi/MonitoringServiceModels/ErrorRate.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatabaseApi.MonitoringServiceModels
+{
+    public class ErrorRate
+    {
+        public DateTime time_Stamp { get; set; }
+    }
+}
---
+++ DatabaseApi/MonitoringServiceModels/Login.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatabaseApi.MonitoringServiceModels
+{
+    public class Login
+    {
+        public DateTime time_Stamp { get; set; }
+        public string accountName { get; set; }
+        public string accountType { get; set; }
+    }
+}
