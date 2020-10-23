35ac7ca0bf27d25c05b5e110fcaacccc28568b3c
Jordan C <nykezgaming@live.com>
2020-10-23 01:29:24 -0400

    load the roles into the claim when user logins

--- DatabaseApi/Controllers/AuthController.cs
+++ DatabaseApi/Controllers/AuthController.cs
@@ -19,103 +19,115 @@ namespace DatabaseApi.Controllers
     {
         private readonly UserManager<IdentityUser> _userManager;
         private readonly JwtBearerTokenSettings _jwtTokenOptions;
+        
         public AuthController(IOptions<JwtBearerTokenSettings> jwtTokenOptions, UserManager<IdentityUser> userManager)
         {
             _jwtTokenOptions = jwtTokenOptions.Value;
             _userManager = userManager;
         }
 
-        [HttpPost]
-        [Route("register")]
-        public async Task<IActionResult> Register([FromBody] UserDetails userDetails)
+    [HttpPost]
+    [Route("register")]
+    public async Task<IActionResult> Register([FromBody] UserDetails userDetails)
+    {
+        // failed request
+        // model state is wrong or post details is null
+        if (!ModelState.IsValid || userDetails == null)
+            return new BadRequestObjectResult(new { Message = "User Registration Failed" });
+
+        var identityUser = new IdentityUser() { UserName = userDetails.Username, Email = userDetails.Email };
+        var result = await _userManager.CreateAsync(identityUser, userDetails.Password);
+
+        // user registration failed in some sort
+        // pass the errors back to the frontend
+        if (!result.Succeeded)
         {
-            // failed request
-            // model state is wrong or post details is null
-            if (!ModelState.IsValid || userDetails == null)
-                return new BadRequestObjectResult(new {Message = "User Registration Failed"});
-
-            var identityUser = new IdentityUser() { UserName = userDetails.Username, Email = userDetails.Email};
-            var result = await _userManager.CreateAsync(identityUser, userDetails.Password);
-
-            // user registration failed in some sort
-            // pass the errors back to the frontend
-            if (!result.Succeeded)
+            var dictionary = new ModelStateDictionary();
+            foreach (IdentityError error in result.Errors)
             {
-                var dictionary = new ModelStateDictionary();
-                foreach (IdentityError error in result.Errors)
-                {
-                    dictionary.AddModelError(error.Code, error.Description);
-                }
-
-                return new BadRequestObjectResult(new { Message = "User Registration Failed", Errors = dictionary});
+                dictionary.AddModelError(error.Code, error.Description);
             }
 
-            return Ok(new {Message = "Registration Successful"});
-
+            return new BadRequestObjectResult(new { Message = "User Registration Failed", Errors = dictionary });
         }
 
-        [HttpPost]
-        [Route("login")]
-        public async Task<IActionResult> Login([FromBody] LoginCredentials credentials)
-        {
-            IdentityUser identityUser;
+        await _userManager.AddToRoleAsync(identityUser, "User");
 
-            if (!ModelState.IsValid 
-                || credentials == null
-                || (identityUser = await ValidateUser(credentials)) == null)
+
+        return Ok(new { Message = "Registration Successful" });
+
+    }
+
+    [HttpPost]
+    [Route("login")]
+    public async Task<IActionResult> Login([FromBody] LoginCredentials credentials)
+    {
+        IdentityUser identityUser;
+
+        if (!ModelState.IsValid
+            || credentials == null
+            || (identityUser = await ValidateUser(credentials)) == null)
+        {
+            return new BadRequestObjectResult(new { Message = "Login failed" });
+        }
+
+        var token = GenerateToken(identityUser).Result;
+        return Ok(new { Token = token, Message = "success" });
+    }
+
+    [HttpPost]
+    [Route("logout")]
+    public async Task<IActionResult> Logout()
+    {
+        // TODO: log user out
+        return Ok(new { Token = "", Message = "Logged Out" });
+    }
+
+    private async Task<IdentityUser> ValidateUser(LoginCredentials credentials)
+    {
+        // attempt to find the user by username
+        var identityUser = await _userManager.FindByNameAsync(credentials.Username);
+
+        if (identityUser != null)
+        {
+            // do passwords match?
+            var result = _userManager.PasswordHasher.VerifyHashedPassword(identityUser, identityUser.PasswordHash, credentials.Password);
+            return result == PasswordVerificationResult.Failed ? null : identityUser;
+        }
+
+        return null;
+    }
+
+    private async Task<object> GenerateToken(IdentityUser identityUser)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var key = Encoding.ASCII.GetBytes(_jwtTokenOptions.SecretKey);
+
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(new Claim[]
             {
-                return new BadRequestObjectResult(new { Message = "Login failed" });
-            }
-
-            var token = GenerateToken(identityUser);
-            return Ok(new {Token = token, Message = "success"});
-        }
-
-        [HttpPost]
-        [Route("logout")]
-        public async Task<IActionResult> Logout()
-        {
-            // TODO: log user out
-            return Ok(new { Token = "", Message = "Logged Out" });
-        }
-
-        private async Task<IdentityUser> ValidateUser(LoginCredentials credentials)
-        {
-            // attempt to find the user by username
-            var identityUser = await _userManager.FindByNameAsync(credentials.Username);
-
-            if (identityUser != null)
-            {
-                // do passwords match?
-                var result = _userManager.PasswordHasher.VerifyHashedPassword(identityUser, identityUser.PasswordHash, credentials.Password);
-                return result == PasswordVerificationResult.Failed ? null : identityUser;
-            }
-
-            return null;
-        }
-
-        private object GenerateToken(IdentityUser identityUser)
-        {
-            var tokenHandler = new JwtSecurityTokenHandler();
-            var key = Encoding.ASCII.GetBytes(_jwtTokenOptions.SecretKey);
-
-            var tokenDescriptor = new SecurityTokenDescriptor
-            {
-                Subject = new ClaimsIdentity(new Claim[]
-                {
                     new Claim(ClaimTypes.Name, identityUser.UserName.ToString()),
                     new Claim(ClaimTypes.Email, identityUser.Email),
-                    new Claim(ClaimTypes.Role, "User"),
-                }),
-                Expires = DateTime.UtcNow.AddSeconds(_jwtTokenOptions.ExpiryTimeInSeconds),
-                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature),
-                Audience = _jwtTokenOptions.Audience,
-                Issuer = _jwtTokenOptions.Issuer
-            };
+            }),
+            Expires = DateTime.UtcNow.AddSeconds(_jwtTokenOptions.ExpiryTimeInSeconds),
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature),
+            Audience = _jwtTokenOptions.Audience,
+            Issuer = _jwtTokenOptions.Issuer
+        };
 
-            var token = tokenHandler.CreateToken(tokenDescriptor);
+        // fetch any roles for this user
+        var roles = await _userManager.GetRolesAsync(identityUser);
 
-            return tokenHandler.WriteToken(token);
+        // add the roles to the JWT
+        foreach (var roleName in roles)
+        {
+            tokenDescriptor.Subject.AddClaim(new Claim(ClaimTypes.Role, roleName));
         }
+        
+        var jwt_token = tokenHandler.CreateToken(tokenDescriptor);
+
+        return tokenHandler.WriteToken(jwt_token);
     }
+}
 }
