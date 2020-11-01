a4de91a6d246adf526ce6ccc3bf613c76e7f1a95
Jordan C <nykezgaming@live.com>
2020-11-01 01:47:50 -0500

    Create UsersController.cs

---
+++ DatabaseApi/Controllers/UsersController.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using AutoMapper;
+using System.Security.Claims;
+
+namespace DatabaseApi.Controllers
+{
+    public class UsersController : ControllerBase
+    {
+        private readonly BikeShop_Context _context;
+        private readonly IMapper _autoMapper;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public UsersController(BikeShop_Context context, IMapper autoMapper, UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+            _autoMapper = autoMapper;
+            _context = context;
+
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<IdentityUser>>> GetAll()
+        {
+            return Ok(await _context.Users.ToListAsync());
+        }
+        
+        [HttpGet("{id}")]
+        public async Task<ActionResult> GetUserById(string id)
+        {
+            var user = _userManager.Users.FirstOrDefaultAsync(u => u.UserName == id);
+
+            if (user == null)
+            {
+                return NoContent();
+            }
+
+            return Ok(user);
+        }
+    }
+}
