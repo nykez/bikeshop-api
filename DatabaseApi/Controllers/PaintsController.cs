a51521b60f6ef4136ab1036444753b650c88da6d
Harrison Pollitte <pollitteh@etsu.edu>
2020-11-09 11:01:22 -0500

    Altered PaintsController to use DTos

--- DatabaseApi/Controllers/PaintsController.cs
+++ DatabaseApi/Controllers/PaintsController.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using DatabaseApi;
+using DatabaseApi.Dtos;
 
 namespace DatabaseApi.Controllers
 {
@@ -14,7 +16,7 @@ namespace DatabaseApi.Controllers
     public class PaintsController : ControllerBase
     {
         private readonly BikeShop_Context _context;
-
+        private readonly IMapper _mapper;
         public PaintsController(BikeShop_Context context)
         {
             _context = context;
@@ -52,44 +54,30 @@ namespace DatabaseApi.Controllers
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for
         // more details see https://aka.ms/RazorPagesCRUD.
         [HttpPut("{id}")]
-        public async Task<IActionResult> PutPaint(int id, Paint paint)
-        {
-            if (id != paint.Paintid)
-            {
-                return BadRequest();
-            }
+        public async Task<IActionResult> PutPaint(int id, [FromBody] PaintToUpdate paint) {
+            var toUpdatePaint = await _context.Paint.FirstOrDefaultAsync(b => b.Paintid == id);
+            
+            if (toUpdatePaint == null) return NoContent();
 
-            _context.Entry(paint).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!PaintExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
+            _mapper.Map(paint, toUpdatePaint);
+            await _context.SaveChangesAsync();
+            return Ok();
         }
 
         // POST: api/Paints
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for
         // more details see https://aka.ms/RazorPagesCRUD.
         [HttpPost]
-        public async Task<ActionResult<Paint>> PostPaint(Paint paint)
+        public async Task<ActionResult<Paint>> PostPaint([FromBody] PaintToCreate paint)
         {
-            _context.Paint.Add(paint);
-            await _context.SaveChangesAsync();
+            if (!ModelState.IsValid) return BadRequest();
 
-            return CreatedAtAction("GetPaint", new { id = paint.Paintid }, paint);
+            var newPaint = _mapper.Map<Paint>(paint);
+            _context.Paint.Add(newPaint);
+
+            await _context.SaveChangesAsync();
+            return Ok(newPaint);
+
         }
 
         // DELETE: api/Paints/5
@@ -97,12 +85,11 @@ namespace DatabaseApi.Controllers
         public async Task<ActionResult<Paint>> DeletePaint(int id)
         {
             var paint = await _context.Paint.FindAsync(id);
-            if (paint == null)
-            {
-                return NotFound();
-            }
+            
+            if (paint == null) return NotFound();
 
             _context.Paint.Remove(paint);
+            
             await _context.SaveChangesAsync();
 
             return paint;
---
+++ DatabaseApi/Dtos/PaintToUpdate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DatabaseApi.Dtos {
+    /// <summary>
+    /// DTO object for updating existing entries of <see cref="Paint"/> in the database.
+    /// </summary>
+    public class PaintToUpdate {
+        public string Colorname { get; set; }
+        public string Colorstyle { get; set; }
+        public string Colorlist { get; set; }
+
+
+        public DateTime? Dateintroduced { get; set; }
+        public DateTime? Datediscontinued { get; set; }
+    }
+}
